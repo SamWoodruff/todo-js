613323e5b1d5ad0a419ec53ed1fca98b1b3a9eab
SamWoodruff <49040392+SamWoodruff@users.noreply.github.com>
2019-04-05 15:13:09 -0500

    Click row
    Ability to check an item complete by clicking on anywhere on the row

--- script/todo.js
+++ script/todo.js
@@ -4,15 +4,16 @@ let todos = [
     {id: 3, text: "Get oil change", complete: false},
     {id: 4, text: "Write thank-you notes", complete: false},
 ];
-var count = 0;
+
 function getList(){
     var i;
+    var count = 0;//keep track of sum of uncompleted tasks
     for(i = 0; i < todos.length; i++){
 		var style;
 		if(todos[i].complete == true){
-			document.getElementById('main-todo-list').innerHTML += '<div class="todo complete" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo-checkbox" checked/>'+ todos[i].text +'</span></div>';
+			document.getElementById('main-todo-list').innerHTML += '<div onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo complete" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" class="todo-checkbox" checked/>'+ todos[i].text +'</span></div>';
 		}else{
-			document.getElementById('main-todo-list').innerHTML += '<div class="todo" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo-checkbox" />'+ todos[i].text +'</span></div>';
+			document.getElementById('main-todo-list').innerHTML += '<div onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" class="todo-checkbox" />'+ todos[i].text +'</span></div>';
             count++;
 		}
     }
@@ -20,16 +21,32 @@ function getList(){
 }
 
 function checkItem(e){
-    if (todos[e].complete == false) {
+    if (todos[e].complete == false) {//Item is complete
         todos[e].complete = true;
-        document.getElementById(e).className = "todo complete";
-            count--;
-    }else if (todos[e].complete == true) {
+    }else if (todos[e].complete == true){//Item is uncomplete
         todos[e].complete = false;
-        document.getElementById(e).className = "todo";
-        count++;
     }
-    document.getElementById("remaining-count").innerHTML = count.toString();
+    count = 0;
+    document.getElementById('main-todo-list').innerHTML='';
+    getList();
+    
 }
+
+var input = document.getElementById("newTodo");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13){
+        event.preventDefault();
+        var todo = document.getElementById("newTodo").value;
+        if(todo != ""){
+            todos.push({id: (todos.length + 1), text: todo, complete: false});
+            document.getElementById('main-todo-list').innerHTML='';
+            getList();
+            document.getElementById("newTodo").value = "";//Reset textbox
+        }else{
+            alert("You forgot to enter your task!");
+        }
+    }
+});
+
 
 window.onload=getList();
