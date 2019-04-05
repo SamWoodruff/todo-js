91c415367ae1cd06d22cadf51315a9a3375458e8
SamWoodruff <49040392+SamWoodruff@users.noreply.github.com>
2019-04-05 14:35:08 -0500

    Counts active todos

--- script/todo.js
+++ script/todo.js
@@ -4,7 +4,7 @@ let todos = [
     {id: 3, text: "Get oil change", complete: false},
     {id: 4, text: "Write thank-you notes", complete: false},
 ];
-
+var count = 0;
 function getList(){
     var i;
     for(i = 0; i < todos.length; i++){
@@ -13,19 +13,23 @@ function getList(){
 			document.getElementById('main-todo-list').innerHTML += '<div class="todo complete" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo-checkbox" checked/>'+ todos[i].text +'</span></div>';
 		}else{
 			document.getElementById('main-todo-list').innerHTML += '<div class="todo" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo-checkbox" />'+ todos[i].text +'</span></div>';
+            count++;
 		}
     }
+    document.getElementById("remaining-count").innerHTML = count.toString();
 }
 
 function checkItem(e){
-		var index = e;
-		if (todos[e].complete == false) {
-			todos[e].complete = true;
-			document.getElementById(e).className = "todo complete";
-		}else if (todos[e].complete == true) {
-			todos[e].complete = false;
-			document.getElementById(e).className = "todo";
-		}
+    if (todos[e].complete == false) {
+        todos[e].complete = true;
+        document.getElementById(e).className = "todo complete";
+            count--;
+    }else if (todos[e].complete == true) {
+        todos[e].complete = false;
+        document.getElementById(e).className = "todo";
+        count++;
+    }
+    document.getElementById("remaining-count").innerHTML = count.toString();
 }
 
 window.onload=getList();
