926bc0361d87849a80234bd584ff161d26be44d3
SamWoodruff <49040392+SamWoodruff@users.noreply.github.com>
2019-05-08 10:00:41 -0500

    Ability to show and display completed items
    Can show or hide completed items on button click. Lists may be empty if all items are completed.

--- script/todo.js
+++ script/todo.js
@@ -5,19 +5,34 @@ let todos = [
     {id: 4, text: "Write thank-you notes", complete: false},
 ];
 
+var show = 1;
 function getList(){
     var i;
+     document.getElementById('main-todo-list').innerHTML='';
     var count = 0;//keep track of sum of uncompleted tasks
     for(i = 0; i < todos.length; i++){
 		var style;
-		if(todos[i].complete == true){
+		if(show == 1 && todos[i].complete == true){
 			document.getElementById('main-todo-list').innerHTML += '<div onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo complete" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" class="todo-checkbox" checked/>'+ todos[i].text +'</span></div>';
-		}else{
+		}else if(todos[i].complete == false){
 			document.getElementById('main-todo-list').innerHTML += '<div onclick="checkItem('+ (todos[i].id - 1) + ')" class="todo" id="'+ (todos[i].id - 1) +'"><span class="todo-text"><input type="checkbox" class="todo-checkbox" />'+ todos[i].text +'</span></div>';
             count++;
 		}
     }
     document.getElementById("remaining-count").innerHTML = count.toString();
+}
+
+function showCompleted(){
+   var status = document.getElementById("status").innerHTML; 
+    if(status == "Hide completed items"){
+        document.getElementById("status").innerHTML= "Show completed items";
+        show = 0;    
+    }
+    else if(status == "Show completed items"){
+        document.getElementById("status").innerHTML= "Hide completed items";
+        show = 1;
+    }
+    getList();
 }
 
 function checkItem(e){
@@ -26,10 +41,6 @@ function checkItem(e){
     }else if (todos[e].complete == true){//Item is uncomplete
         todos[e].complete = false;
     }
-    document.getElementById('main-todo-list').innerHTML='';
-    getList();
-    count = 0;
-    document.getElementById('main-todo-list').innerHTML='';
     getList();
     
 }
@@ -41,8 +52,6 @@ input.addEventListener("keyup", function(event) {
         var todo = document.getElementById("newTodo").value;
         if(todo != ""){
             todos.push({id: (todos.length + 1), text: todo, complete: false});
-            document.getElementById('main-todo-list').innerHTML='';
-			count = 0;
             getList();
             document.getElementById("newTodo").value = "";//Reset textbox
         }else{
